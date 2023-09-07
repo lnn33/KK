c4c17ced2d3b771cce87bc1f7187dc27efcc5a63
lnn33 <133614979+lnn33@users.noreply.github.com>
2023-09-07 11:07:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,9 +174,9 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
+            <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+  <IconButton icon={<GithubIcon />} shadow />
+</a>
           </div>
         </div>
         <div>
