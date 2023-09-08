3f8c26c1a73faeef508e1ef81c0375a975345844
lnn33 <133614979+lnn33@users.noreply.github.com>
2023-09-08 14:21:42 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          LnnLee ChatGPT
+          Wendy Wiki ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Your own AI assistant.
+          Your own AI assistant. Happy Birthday
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
