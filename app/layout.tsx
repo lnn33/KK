42ec97e6a26381d87d64d56aa8e6f70a70d37710
lnn33 <133614979+lnn33@users.noreply.github.com>
2023-09-08 14:31:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "LnnLee ChatGPT",
+  title: "Wendy Wiki ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "LnnLee ChatGPT",
+    title: "Wendy Wiki ChatGPT",
     statusBarStyle: "default",
   },
 };
